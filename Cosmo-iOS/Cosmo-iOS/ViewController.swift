1ac732751568dcbeb37c3c552128c5344b4dfb62
owllwo98 <sloopy98@naver.com>
2025-03-04 13:55:48 +0900

    *first commit

---
+++ Cosmo-iOS/Cosmo-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cosmo-iOS
+//
+//  Created by 변정훈 on 3/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
